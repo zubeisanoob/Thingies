3943afee5bd2ab81167d3006c3fe3560789b87e9
Zubin <45184951+zubeisanoob@users.noreply.github.com>
2020-11-18 09:29:31 -0800

    Update Thingy.java

--- Thingy.java
+++ Thingy.java
@@ -5,5 +5,16 @@ class Thingy
   {
     myX = myY = 0;
   }
-  /* your java code for "getters" and "setters" here */
+  int getX() {
+    return myX;
+  }
+  int getY() {
+    return myY;
+  }
+  public void setX(int x_) {
+    myX = x_;
+  }
+  public void setY(int y_) {
+    myY = y_;
+  }
 }
